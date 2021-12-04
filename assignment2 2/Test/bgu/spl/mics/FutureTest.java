4e78be910311fd2deb4eea0b90b3282b0f708666
shuOfir <95532512+shuOfir@users.noreply.github.com>
2021-12-04 20:36:41 +0200

    Update FutureTest.java

--- assignment2 2/Test/bgu/spl/mics/FutureTest.java
+++ assignment2 2/Test/bgu/spl/mics/FutureTest.java
@@ -5,20 +5,61 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FutureTest {
-
-    @Test
-    public void get() {
-    }
-
-    @Test
-    public void resolve() {
-    }
-
-    @Test
-    public void isDone() {
-    }
-
+//ofir added 
+//************************************************************************************
     @Test
     public void testGet() {
+        Thread th1 = new Thread(new Runnable() {
+            public void run() {
+                String result = (String) future.get();
+                assertEquals(result, "result");
+            }
+        });
+
+        Thread th2 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    assertFalse(future.isDone());
+                }
+                future.resolve("result");
+                assertTrue(future.isDone());
+            }
+        });
+        th2.start();
+
+        th1.start();
     }
+    @Test
+    public void testResolve(){
+        testGet(); // iff situation with testGet(no way to test get without testing is)
+    }
+
+    @Test
+    public void testIsDone(){
+        assertFalse(future.isDone());
+        future.resolve("T result changed");
+        assertTrue(future.isDone());
+    }
+
+    public void testTestGet() {
+        Thread publisherTest = new Thread(new Runnable() {
+            public void run() {
+                future.get(15, TimeUnit.SECONDS);
+            }
+        });
+        publisherTest.start();
+        try {
+            Thread.sleep(15*1000);
+            assertFalse(publisherTest.isAlive());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+//************************************************************************************
 }
